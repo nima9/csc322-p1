8c222585c4de4cc1f88d376f9b2e2a45d68af130
Nima Mohajeri <nima.mohajeri0@gmail.com>
2024-02-08 16:38:29 -0800

    Feat: Added the two

---
+++ sat2sud.py
@@ -0,0 +1,5 @@
+"""
+sat2sud reads the output produced by miniSAT for a given puzzle 
+instance and converts it back into a solved Sudoku puzzle (as a text file, with newlines for readability.)
+
+"""
---
+++ sud2sat.py
@@ -0,0 +1,7 @@
+"""
+sud2sat reads a single Sudoku puzzle (in some specified text format) 
+and converts it to a CNF formula suitable for input to the miniSAT SAT solver (described below.) 
+For the basic task, you only need to consider the “minimal” enoding 
+of puzzles as CNF formulas (described in class).
+
+"""
