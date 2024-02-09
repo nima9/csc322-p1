c9708611448c2ca9ac2a00250f5c4ce68eeda2da
lambert-alex <alex.m.lambert@icloud.com>
2024-02-09 14:17:58 -0800

    add file output parser and update ex2.txt with ?

--- sud2sat.py
+++ sud2sat.py
@@ -1,19 +1,38 @@
 """
 sud2sat reads a single Sudoku puzzle (in some specified text format) 
 and converts it to a CNF formula suitable for input to the miniSAT SAT solver (described below.) 
-For the basic task, you only need to consider the “minimal” enoding 
+For the basic task, you only need to consider the minimal enoding 
 of puzzles as CNF formulas (described in class).
 
 """
-
 import argparse
 
 
 def main(args):
     sudoku = read_sudoku_puzzle(args.puzzle)
+    solution = {
+                'num_variables': 0, # int representing num of variables
+                'num_clauses': 0, # int representing num of clauses
+                'clauses': [] # 2d array of ints each int reps a variable ie 1 = x_1 terminate each clause line with 0
+                }
 
-    for row in sudoku:
-        print(row)
+    dimacs_format(solution, args.output_file)
+
+
+def dimacs_format(solution, output_file):
+    with open(output_file, "w") as file:
+        # set up file header
+        file.write(f"p cnf {solution['num_variables']} {solution['num_clauses']}\n")
+
+        ''' 
+        write each clause line ie [[1,2,-3],[4,5,6]] is
+        1 2 -3
+        4 5 6
+        which represents
+        (x1 | x2 | x3) & (x4 | x5 | x6)
+        '''
+        for clause in solution['clauses']:
+            file.write(f"{' '.join(map(str,clause))}\n")
 
 
 def read_sudoku_puzzle(file_path):
@@ -34,11 +53,9 @@ def read_sudoku_puzzle(file_path):
     return puzzle
 
 
-# Example usage:
-
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("puzzle", type=str)
+    parser.add_argument("output_file", type=str)
     args = parser.parse_args()
     main(args)
