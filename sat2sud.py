3152f58653d79749f74eef428156e1915da5f046
Nima Mohajeri <nima.mohajeri0@gmail.com>
2024-02-08 17:26:25 -0800

    added a comment

--- sat2sud.py
+++ sat2sud.py
@@ -2,6 +2,8 @@
 sat2sud reads the output produced by miniSAT for a given puzzle 
 instance and converts it back into a solved Sudoku puzzle (as a text file, with newlines for readability.)
 
+sudoku empty spaces can be 0, ., *, and ?
+
 """
 
 import argparse
