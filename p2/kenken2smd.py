001b165864fa930c301db0c2b7361813902d9936
lambert-alex <alex.m.lambert@icloud.com>
2024-03-18 17:06:05 -0700

    more details

--- p2/kenken2smd.py
+++ p2/kenken2smd.py
@@ -11,8 +11,16 @@ def main(args):
 def parse_puzzle(puzzle):
     parse_dict = {
                     'var': [], #list of all variables
-                    'row': [], # vars in each row
-                    'columns': [], # vars in each columns
+                    'row': [{
+                                'line_one': -1,
+                                'line_two': -1,
+                                'vars': []
+                            }], # (assert (distinct V0 V1 V2 V3 V4 V5 V6 V7 V8 )) ; line What is this!!!! ???0 1
+                    'columns': [{
+                                    'line_one': -1,
+                                    'line_two': -1,
+                                    'vars': []
+                                }], # vars in each columns
                     'region': [{
                                 'operator': '', # operator
                                 'equals': -1, # int for what equals
@@ -25,17 +33,20 @@ def parse_puzzle(puzzle):
 
 def write_ans(output_file, encoded):
     with open(output_file, "w") as file:
-        file.write("(set-logic UFNIA)")
-        file.write("(set-option :produce-models true)")
-        file.write("set-option :produce-assignments true)")
+        file.write("(set-logic UFNIA)\n")
+        file.write("(set-option :produce-models true)\n")
+        file.write("set-option :produce-assignments true)\n")
         for var in encoded['var']:
-            file.write(f"(declare-const {var} Int)")
+            file.write(f"(declare-const {var} Int)\n")
         for row in encoded['row']:
-            file.write(f"(assert (distinct {row} )) ; line {} {}")
+            file.write(f"(assert (distinct {row['vars']} )) ; line {row['line_one']} {row['line_two']}\n") # maybe comments? need to figure out the numbers mean in line ? ?
         for column in encoded['column']:
-            file.write(f"(assert (distinct {column} )) ; line {} {}")
+            file.write(f"(assert (distinct {column['vars']} )) ; line {row['line_one']} {row['line_two']}\n") # need to figure out what the numbers mean in line ? ?
         for region in encoded['regions']:
-            file.write(f"(assert ({region['operator']}))")
+            if region['operator'] in  ["-", '/']:
+                file.write(f"(assert (or (= {region['equals']} {''})) ; Region {region['name']}\n") # needs more work needs combinatorial combination of vars
+            else:
+                file.write(f"(assert (= {region['equals']} ({region['operator']} {region['vars']}))) ; Region {region['name']}\n")
 
         file.write("(check-sat)")
         file.write(f"(get-value ({encoded['var']}))")
